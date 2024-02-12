6739d81bdb6ebec2c42232f10fa948b53e5a26eb
uchaiaye <uchhai53@gmail.com>
2024-02-12 20:07:47 +0600

    fixed some design problem

--- src/components/SearchSortAdd.js
+++ src/components/SearchSortAdd.js
@@ -30,19 +30,19 @@ const SearchSortAdd = () => {
   };
 
   return (
-  <div className="bg-purple-100 rounded-lg shadow-md flex items-center justify-between p-4 mt-10 sticky top-0">
+    <div className="bg-purple-100 rounded-lg shadow-md flex flex-col sm:flex-row items-center justify-between p-4 mt-10 sticky top-0">
       {/* Search Field */}
       <input
         type="text"
         placeholder="Search"
-        className="border-none bg-purple-100 focus:outline-none flex-grow mr-2"
+        className="border-none bg-purple-100 focus:outline-none flex-grow mr-2 sm:mb-0 sm:mr-0"
         onChange={(e) => {
           setSearch(e.target.value);
         }}
       />
       {/* Sorting Select Field */}
       <select
-        className="border-none bg-purple-100 focus:outline-none w-24 mr-2"
+        className="border-none bg-purple-100 focus:outline-none w-full sm:w-24 mr-2"
         value={sortBy || "default"}
         onChange={handleSortChange}
       >
@@ -53,9 +53,9 @@ const SearchSortAdd = () => {
       </select>
       {/* Add Button */}
       <Link to="/addUser">
-      <button className="bg-purple-500 text-white px-4 py-2 rounded hover:bg-purple-600 focus:outline-none">
-        Add User
-      </button>
+        <button className="bg-purple-500 text-white px-3 py-1 rounded hover:bg-purple-600 focus:outline-none sm:px-4 sm:py-2">
+          Add User
+        </button>
       </Link>
     </div>
   );
